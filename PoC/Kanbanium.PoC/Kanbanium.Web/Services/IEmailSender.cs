ecc567442e62be4814134663e815df1814924d2d
Techbuzzz <me@techbuzzz.me>
2016-05-12 10:42:54 +0300

    Start develop PoC

---
+++ PoC/Kanbanium.PoC/Kanbanium.Web/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kanbanium.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ PoC/Kanbanium.PoC/Kanbanium.Web/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kanbanium.Web.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
